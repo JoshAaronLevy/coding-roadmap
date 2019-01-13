54b860c1040984d355cfa7b5032d86a6f37be2ae
Josh <joshaaronlevy@gmail.com>
2019-01-13 10:35:26 -0700

    Set up foundation with material

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
 new Vue({
   router,
   store,
