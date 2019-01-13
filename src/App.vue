9f746bf3126b7945fd545f460b0e9cf014d31102
Josh <joshaaronlevy@gmail.com>
2019-01-13 11:25:53 -0700

    Got post list on home page

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
       </md-app-drawer>
 
       <md-app-content>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
+        <router-view/>
       </md-app-content>
     </md-app>
   </div>
