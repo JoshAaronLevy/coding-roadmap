d493d6660e676e0215f48f40e6b45bed857b4c31
Josh <joshaaronlevy@gmail.com>
2019-01-13 11:18:28 -0700

    Cleaned up header

--- src/App.vue
+++ src/App.vue
@@ -21,26 +21,10 @@
             <md-icon>move_to_inbox</md-icon>
             <span class="md-list-item-text">Inbox</span>
           </md-list-item>
-
-          <md-list-item>
-            <md-icon>send</md-icon>
-            <span class="md-list-item-text">Sent Mail</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>delete</md-icon>
-            <span class="md-list-item-text">Trash</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>error</md-icon>
-            <span class="md-list-item-text">Spam</span>
-          </md-list-item>
         </md-list>
       </md-app-drawer>
 
       <md-app-content>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.</p>
       </md-app-content>
     </md-app>
---
+++ src/buttercms.js
@@ -0,0 +1,2 @@
+import Butter from 'buttercms'
+export const butter = Butter('9850fc104dc0353b9437eb965f090f8386f9f1ee')
